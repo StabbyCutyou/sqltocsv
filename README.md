901c1a66ad96852538827990288850c418b029a1
StabbyCutyou <smattkelly@gmail.com>
2015-09-27 23:39:35 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+SQL To CSV
+============
+
+This is a utility to make use of golangs sql package and it's streaming capabilities,
+for exporting large result sets out of an RDBMS and into a text file.
+
+Curently, it supports TSV and mysql. Additional support coming for setting the delimeter,
+as well as additional databases.
+
+To run it, call from bash like so:
+
+```bash
+STC_DBADAPTER="mysql" STC_CONNSTRING="root@tcp(localhost:3306)/mydb" STC_QUERY="SELECT * FROM table" STC_OUTPUTFILE="data.csv" go run sqltocsv.go
+```
+
+TODO
+=====
+Proper Readme
+Postgres support
+Variable delimeter
+
+LICENSE
+=========
+Apache v2 - See LICENSE
---
+++ sqltocsv.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	// blank to add the mysql driver
+	"github.com/StabbyCutyou/sqlx"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// Config is
+type Config struct {
+	dbAdapter  string
+	connString string
+	sqlQuery   string
+	outputFile string
+}
+
+func main() {
+	cfg := getConfig()
+	db, err := sqlx.Open(cfg.dbAdapter, cfg.connString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	results, err := db.Queryx(cfg.sqlQuery)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	output, err := os.Create(cfg.outputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	csvWriter := csv.NewWriter(output)
+	csvWriter.Comma = 0x0009
+	firstLine := true
+	for results.Next() {
+		row, cols, err := results.SliceScanWithColumns()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if firstLine {
+			firstLine = false
+			csvWriter.Write(cols)
+		}
+
+		rowStrings := make([]string, len(row))
+		// It seems for mysql, the case is always []byte of a string?
+		for i, col := range row {
+			//log.Print(reflect.TypeOf(col))
+			switch col.(type) {
+			case float64:
+				rowStrings[i] = strconv.FormatFloat(col.(float64), 'f', 6, 64)
+			case int64:
+				rowStrings[i] = strconv.FormatInt(col.(int64), 10)
+			case bool:
+				rowStrings[i] = strconv.FormatBool(col.(bool))
+			case []byte:
+				rowStrings[i] = string(col.([]byte))
+			case string:
+				rowStrings[i] = col.(string)
+			case time.Time:
+				rowStrings[i] = col.(time.Time).String()
+			case nil:
+				rowStrings[i] = "NULL"
+			default:
+				log.Print(col)
+			}
+		}
+		csvWriter.Write(rowStrings)
+	}
+
+	csvWriter.Flush()
+	output.Close()
+}
+
+func getConfig() *Config {
+	cfg := &Config{
+		dbAdapter:  os.Getenv("STC_DBADAPTER"),
+		connString: os.Getenv("STC_CONNSTRING"),
+		sqlQuery:   os.Getenv("STC_QUERY"),
+		outputFile: os.Getenv("STC_OUTPUTFILE"),
+	}
+	if cfg.dbAdapter == "" {
+		log.Fatal("You must provide a connection string via STC_DBADAPTER")
+	}
+	if cfg.connString == "" {
+		log.Fatal("You must provide a connection string via STC_CONNSTRING")
+	}
+	if cfg.sqlQuery == "" {
+		log.Fatal("You must provide a query to run via STC_QUERY")
+	}
+	if cfg.outputFile == "" {
+		log.Fatal("You must provide an output file via STC_OUTPUTFILE")
+	}
+
+	return cfg
+}
