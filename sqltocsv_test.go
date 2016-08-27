4a943903540ee7bd1a78a220c575f3ad783ef09f
StabbyCutyou <smattkelly@gmail.com>
2016-08-27 09:03:42 -0400

    Shell of tests

---
+++ sqltocsv_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+)
+
+func setupMySQL() {
+
+}
+
+func tearDownMySQL() {
+
+}
+
+func setupPostgreSQL() {
+
+}
+
+func tearDownPostgreSQL() {
+
+}
+
+func TestMySQL(t *testing.T) {
+
+}
+
+func TestPostgreSQL(t *testing.T) {
+
+}
+
+func TestUnsupportedDriver(t *testing.T) {
+
+}
